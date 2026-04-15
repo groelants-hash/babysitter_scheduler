fc6b3cc5fb6f116b2399f2419bb7f12986f54a68
groelants-hash <groelants@me.com>
2026-04-15 12:05:21 +0200

    fix: match Redis key names in API routes

--- api/data.js
+++ api/data.js
@@ -5,7 +5,7 @@ const redis = new Redis({
   token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
 
-const KEY = "babysitter-app-data";
+const KEY = "babysitter:slots";
 
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
--- api/users.js
+++ api/users.js
@@ -5,7 +5,7 @@ const redis = new Redis({
   token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
 
-const KEY = "babysitter-app-users";
+const KEY = "babysitter:users";
 
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
